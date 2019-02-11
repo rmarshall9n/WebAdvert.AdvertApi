be2c037f0111401b2f595347c0daf50768f52544
Ryan Marshall <rmarshall9n@gmail.com>
2019-02-11 22:38:34 +0000

    Added models and hooked up basic api endpoints with dynamo db.

---
+++ AdvertApi/Controllers/Advert.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdvertApi.Models;
+using AdvertApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvertApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/adverts")]
+    public class Advert : ControllerBase
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+
+        public Advert(IAdvertStorageService advertStorageService)
+        {
+            _advertStorageService = advertStorageService;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId;
+            try
+            {
+                recordId = await _advertStorageService.Add(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+            }
+
+            return StatusCode(201, new CreateAdvertResponse { Id = recordId });
+        }
+
+        [HttpPut]
+        [Route("Confirm")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            try
+            {
+                await _advertStorageService.Confirm(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+            }
+
+            return new OkResult();
+        }
+    }
+}
